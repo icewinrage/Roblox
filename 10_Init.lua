cc4e67afe4f1676820bd3b8de28526b817db111a
icewinrage <nurelm.1000@gmail.com>
2026-09-22 23:35:31 +0600

    10_Init.lua

--- 10_Init.lua
+++ 10_Init.lua
@@ -301,6 +301,22 @@ function Init.Run()
         pcall(OnlineTab.PopulateTab)
     end
 
+    local Chat = _G.Venture.Chat
+    if Chat then
+        pcall(Chat.PopulateTab)
+    end
+
+    local Announce = _G.Venture.Announce
+    if Announce then
+        pcall(Announce.PopulateTab)
+        pcall(Announce.Init)
+    end
+
+    local ChatWindow = _G.Venture.ChatWindow
+    if ChatWindow then
+        pcall(ChatWindow.Init)
+    end
+
     RunService.RenderStepped:Connect(function()
         pcall(UpdateESP)
     end)
