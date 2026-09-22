6e42748fe8b0f03166598d3eb9baede6390d22da
icewinrage <nurelm.1000@gmail.com>
2026-09-22 21:32:41 +0600

    09_Supabase.lua

--- 09_Supabase.lua
+++ 09_Supabase.lua
@@ -127,7 +127,7 @@ local function SafeMakeBadge(pl, role)
     label.Position = UDim2.new(0, isOwner and 28 or 24, 0, 0)
     label.Size = UDim2.new(1, isOwner and -32 or -28, 1, 0)
     label.BackgroundTransparency = 1
-    label.Text = isOwner and "DEVELOPER" or "SCRIPT USER"
+    label.Text = isOwner and "SCRIPT DEV" or "SCRIPT USER"
     label.TextColor3 = Color3.fromRGB(255, 255, 255)
     label.TextStrokeTransparency = 0
     label.TextStrokeColor3 = Color3.fromRGB(40, 0, 80)
@@ -248,6 +248,7 @@ local function SendBeacon()
         name = Supa.MY_NAME,
         role = Supa.MY_ROLE,
         job_id = Supa.MY_JOB,
+        place_id = tostring(game.PlaceId),
         last_seen = ISOTime(0),
     }, HEADERS)
 end
